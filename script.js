b6e31c3a5082127b3c5df8ae19a16acd43cec840
Nasir zia <107996170+nasirzia133@users.noreply.github.com>
2023-02-28 00:04:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,136 @@
+// //  1
+//  let num= 5>4;
+//  console.log(num);
+// // 2
+//  let fruit= "apple">"pineapple";
+//  console.log(fruit);
+// // 3
+// let int="2">"12";
+// console.log(int);
+
+// // 4
+
+// let x=null;
+// console.log(x==undefined);
+
+// // 5
+
+
+// let y=null;
+
+// console.log(x===undefined)
+
+// // 6
+
+// let z="\n0\n";
+// console.log(z==null);
+
+// // 7
+
+// let X=+"\n0\n";
+
+// console.log(X===null);
+
+// // 8
+
+// let a=1;
+// let b=1;
+// let c =++a;
+// let d=++b;
+// console.log(c,d);
+
+
+
+// 9
+
+// let a = 2;
+// let x = 1 +(a*=2);
+// console.log(x); 
+
+
+// num1   = Number (prompt("Frist number ?",1));
+// num2  = Number (prompt("sec number ?",2));
+// alert(num1+num2);
+
+// 10
+
+// let a ;
+// let b ;
+// let result;
+
+// result= a + b < 4 ? 'Below' : 'over';
+
+// console.log(result);
+
+
+// 11 
+
+// let a = 2;
+// alert(null||2||undefined);
+
+// 12
+
+// let a = 2;
+// alert(alert(1)||3|| alert(3));
+// console.log();
+
+
+// 13
+// let a = 1;
+
+// alert( 1 && null && 2 );
+
+
+// 14
+// let a = 1;
+
+// alert( alert(1) && alert(2) );
+
+// 15
+// alert( null || 2 && 3 || 4 );
+
+
+// let age;
+// a=prompt("enter the age");
+// if (age >= 14 || age <= 90){
+
+//     alert("age is between them");
+// }
+
+// 17
+
+// if (-1 || 0) alert( 'first' );
+// if (-1 && 0) alert( 'second' );
+// if (null || -1 && 1) alert( 'third' );
+
+// 18
+
+// let browser;
+// let broeser=prompt("enter the browser");
+
+// if (browser === 'Edge') {
+//     alert("You've got the Edge!");
+//   } else if (browser === 'Chrome' || browser === 'Firefox' || browser === 'Safari' || browser === 'Opera') {
+//     alert('Okay we support these browsers too');
+//   } else {
+//     alert('not browser');
+//   }
+
+// 19
+
+// let a = +prompt('a?', '');
+
+//  switch(a){
+//    case 0:
+//      alert("0");
+//     break;
+//     case 2:
+//       alert("2");
+//       break;
+
+//       default :
+//         alert("print one of them");
+//     }
+
+ 
+
